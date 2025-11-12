d180c14f5ba9770095dff5326ee2ff1dd02a1e3e
Pepevida2 <jose.albarracin@ucuenca.edu.ec>
2025-11-12 04:45:05 -0500

    Se creo un sistema de pelotas dentro de cubos

---
+++ Pelota.js
@@ -0,0 +1,42 @@
+class Pelota{
+    constructor(){
+        this.diam = random(50, 150);
+        this.rad = this.diam/2;
+
+        this.posx = random(this.rad, width - this.rad);
+        this.posy = random(this.rad, height - this.rad);
+
+        this.velx=random(-5,5);
+        this.vely=random(-5,5);
+
+        this.nuevoColor = color(random(255,0), random(0,255), random(100, 150), 200);
+    }
+
+    actualizar(){
+        if(this.posx> width - this.rad || this.posx < this.rad){
+            this.velx *= -1;
+        }
+
+        if(this.posy> height - this.rad || this.posy < this.rad){
+            this.vely *= -1;
+    }
+
+    this.posx += this.velx;
+    this.posy += this.vely;
+}
+
+    visualizar(){
+        fill(this.nuevoColor);
+        stroke ("#ffffffff");
+        strokeWeight(2);
+        square(this.posx, this.posy, this.diam);
+        strokeWeight(5);
+        stroke ("#00000010");
+        circle(this.posx, this.posy, this.diam);
+        fill ("#ffffffff");
+        strokeWeight(1);
+        circle(this.posx, this.posy, this.diam/3);
+        
+        
+    }
+}
